b4c872497616e507a636c9e01c716b33a4306e9e
nmctseb <29257008+nmctseb@users.noreply.github.com>
2019-02-07 14:08:49 +0100

    fix hardcoded user/pass in $NOOBS_DIR/os.json

--- export-noobs/00-release/00-run.sh
+++ export-noobs/00-release/00-run.sh
@@ -30,6 +30,8 @@ sed "${NOOBS_DIR}/partitions.json" -i -e "s|ROOT_NOM|${ROOT_NOM}|"
 sed "${NOOBS_DIR}/os.json" -i -e "s|UNRELEASED|${IMG_DATE}|"
 sed "${NOOBS_DIR}/os.json" -i -e "s|NOOBS_NAME|${NOOBS_NAME}|"
 sed "${NOOBS_DIR}/os.json" -i -e "s|NOOBS_DESCRIPTION|${NOOBS_DESCRIPTION}|"
+sed "${NOOBS_DIR}/os.json" -i -e "s|FIRST_USER_NAME|${FIRST_USER_NAME}|"
+sed "${NOOBS_DIR}/os.json" -i -e "s|FIRST_USER_PASS|${FIRST_USER_PASS}|"
 
 sed "${NOOBS_DIR}/release_notes.txt" -i -e "s|UNRELEASED|${IMG_DATE}|"
 
