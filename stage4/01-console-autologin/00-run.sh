9c8a2540c4237cd8b103936ab93419486b92293c
nmctseb <29257008+nmctseb@users.noreply.github.com>
2019-02-11 00:32:19 +0100

    more reorg, fix autologin

--- stage4/01-console-autologin/00-run.sh
+++ stage4/01-console-autologin/00-run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+sed "s/--autologin pi/--autologin ${FIRST_USER_NAME}/g" -i "${ROOTFS_DIR}/etc/systemd/system/autologin@.service"
 
 ln -sf /etc/systemd/system/autologin@.service \
 	"${ROOTFS_DIR}/etc/systemd/system/getty.target.wants/getty@tty1.service"
