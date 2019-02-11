fa88d3c11c0826db0c78eae66f79485639a81cf0
nmctseb <29257008+nmctseb@users.noreply.github.com>
2019-02-11 14:07:29 +0100

    pkg reorg fixes

--- stage1/09-first-user/01-run-chroot.sh
+++ stage1/09-first-user/01-run-chroot.sh
@@ -9,4 +9,4 @@ echo "${FIRST_USER_NAME}:${FIRST_USER_PASS}" | chpasswd
 for GRP in adm dialout cdrom audio users sudo video games plugdev input gpio spi i2c netdev; do
   adduser $FIRST_USER_NAME $GRP
 done
-
+echo "${FIRST_USER_NAME} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/010_${FIRST_USER_NAME}-nopasswd
