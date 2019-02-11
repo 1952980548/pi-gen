33b45c45f79900722be1050bbb6bd2aebccff764
nmctseb <29257008+nmctseb@users.noreply.github.com>
2019-02-11 14:08:10 +0100

    nmct customize fix

--- stage1/90-customize-nmct/00-run-chroot.sh
+++ stage1/90-customize-nmct/00-run-chroot.sh
@@ -1,3 +1,14 @@
 systemctl disable nginx.service
 systemctl disable uwsgi.service
 
+#dpkg-reconfigure -f noninteractive console-setup
+#setupcon --force --save-only -v
+#RPICNF="raspi-config nonint do_"
+#for s in "configure_keyboard be" "boot_wait 1" "boot_splash 1"; do
+#	${RPICNF}${s}
+#done
+
+dpkg-reconfigure keyboard-configuration
+dpkg-reconfigure console-setup
+
+locale-gen
