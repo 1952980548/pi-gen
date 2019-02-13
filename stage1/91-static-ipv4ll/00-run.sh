8634d4dbc71a100dc7ef35a78ef46630a2aa4ef7
nmctseb <29257008+nmctseb@users.noreply.github.com>
2019-02-13 18:05:05 +0100

    raspbian+nmct v1

---
+++ stage1/91-static-ipv4ll/00-run.sh
@@ -0,0 +1,4 @@
+install -v -m 755 files/00-debuglog "${ROOTFS_DIR}/lib/dhcpcd/dhcpcd-hooks/00-debuglog"
+install -v -m 755 files/10-avahi-autoipd "${ROOTFS_DIR}/lib/dhcpcd/dhcpcd-hooks/10-avahi-autoipd"
+install -v -m 644 files/00-debuglog "${ROOTFS_DIR}/lib/dhcpcd/dhcpcd-hooks/00-debuglog"
+install -v -m 644 files/10-avahi-autoipd "${ROOTFS_DIR}/lib/dhcpcd/dhcpcd-hooks/10-avahi-autoipd"
